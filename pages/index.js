901c5c2a839879a2e0d260960ce3777a0552b8db
nanaska <fefe.d43efe@mail.ru>
2022-12-12 18:31:57 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
   return (
       <div>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <title>Яргу</title>
           <meta name="description" content="Ярославский государственный университет имени Павла Григорьевича Демидова. ЯрГУ классический университет, позволяющий получить классическое образование абитуриенту." />
           <link rel="icon" href="/favicon.ico" />
