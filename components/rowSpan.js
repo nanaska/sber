a1203b3dfe25423a7b2b9ef70ae3e732acf54e8d
Denis <denis_shakhov@vk.com>
2022-12-14 01:54:24 +0300

    COMPLETEDISUPOSE

--- components/rowSpan.js
+++ components/rowSpan.js
@@ -28,7 +28,7 @@ export default function RowSpan({ rowClass, deparment, src, link }) {
         x: { delay: 0.25 },
         duration: .25,
       }}
-      className={`w-full relative flex items-center justify-between  mt-4 ${rowClass} h-[48px] md:min-h-[20vh] `}
+      className={`w-full cursor-pointer relative flex items-center justify-between  mt-4 ${rowClass} h-[48px] md:min-h-[20vh] `}
     >
       <div className=" h-full flex items-center justify-center ">
         <span className="text-white ml-10 font-[500] max-w-[200px] md:max-w-full text-[15px] md:text-4xl">
